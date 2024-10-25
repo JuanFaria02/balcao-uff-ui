9c6a5e9c4eaee69a8a3f40575f80e411de39afbb
juan <juanfariacrf@gmail.com>
2024-10-25 01:36:34 -0300

    Criado footer

---
+++ src/components/structure/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from "react-i18next";
+import IconUff from "../../icons/icons"; // Certifique-se de que este caminho está correto
+import { Container, Toolbar } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="static border-t-4">
+            <Container maxWidth="lg" className="pb-7">
+                <Toolbar className="justify-center flex">
+                    <IconUff styles='relative right-8 top-2'/> 
+                </Toolbar>
+            </Container>
+        </div>
+    );
+};
+
+export default React.memo(Footer);
