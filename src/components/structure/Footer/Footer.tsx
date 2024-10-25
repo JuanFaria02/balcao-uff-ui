6d32d47467b44719d562d01e00ff619f72a17f19
juan <juanfariacrf@gmail.com>
2024-10-25 01:48:58 -0300

    Criado links da header

--- src/components/structure/Footer/Footer.tsx
+++ src/components/structure/Footer/Footer.tsx
@@ -1,10 +1,8 @@
-import { useTranslation } from "react-i18next";
 import IconUff from "../../icons/icons"; // Certifique-se de que este caminho está correto
 import { Container, Toolbar } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
-
     return (
         <div className="static border-t-4">
             <Container maxWidth="lg" className="pb-7">
