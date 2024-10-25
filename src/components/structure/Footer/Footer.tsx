2410d2e045fffc3f0a3049f19660a067d2c4050e
juan <juanfariacrf@gmail.com>
2024-10-25 01:38:03 -0300

    Ajuste pontual

--- src/components/structure/Footer/Footer.tsx
+++ src/components/structure/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import { Container, Toolbar } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
-    const { t } = useTranslation();
 
     return (
         <div className="static border-t-4">
